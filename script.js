5b4c3fb86959cc2bcf5df17191f1f013696577ad
Chung Le <lpc-nz@users.noreply.github.com>
2021-03-23 09:40:33 +1300

    Done the project

---
+++ script.js
@@ -0,0 +1,36 @@
+// Dealing With API
+const jokeEl = document.getElementById('joke');
+
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+// using Async Await
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'Application/json',
+    },
+  };
+  const res = await fetch('https://icanhazdadjoke.com/', config);
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
+}
+
+//Using .then to fetch data
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'Application/json',
+//     },
+//   };
+//   fetch('https://icanhazdadjoke.com/', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
